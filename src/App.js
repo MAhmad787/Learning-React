48c1a9a96a259056d608da12a0d46535d6ae2470
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-03-07 22:01:10 +0500

    Components & Props

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Job salary="12000$" position="Senior SDE" company="Amazon" />
+      <hr />
+      <Job salary="15000$" position="Project Manager" company="Google" />
+      <hr />
+      <Job salary="10000$" position="Junior SDE" company="Netflix" />
+      <hr />
+    </div>
+  );
 }
 
+const Job = (props) => {
+  return (
+    <div>
+      <h1>{props.salary}</h1>
+      <h2>{props.position}</h2>
+      <h2>{props.company}</h2>
+    </div>
+  );
+};
 export default App;
