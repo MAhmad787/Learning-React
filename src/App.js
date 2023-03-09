b7780f14ad4dfa4be2c95a660d55961a9774f5f7
Muhammad Ahmad <mahmadmaqbool787@gmail.com>
2023-03-09 15:17:26 +0500

    Add first project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,6 @@
-function App() {
-  return (
-    <div className="App">
-      <Job salary="12000$" position="Senior SDE" company="Amazon" />
-      <hr />
-      <Job salary="15000$" position="Project Manager" company="Google" />
-      <hr />
-      <Job salary="10000$" position="Junior SDE" company="Netflix" />
-      <hr />
-    </div>
-  );
-}
+import ExpenseItem from './components/ExpenseItem';
 
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.salary}</h1>
-      <h2>{props.position}</h2>
-      <h2>{props.company}</h2>
-    </div>
-  );
-};
+function App() {
+  return <ExpenseItem />;
+}
 export default App;
